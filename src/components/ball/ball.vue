76c191ea1718b7c066b45f384cbe1d35231af0c0
wldzhimin <Zhimin.Liu@wolaidai.com>
2017-01-16 11:15:28 +0800

    sell ball

--- src/components/ball/ball.vue
+++ src/components/ball/ball.vue
@@ -76,6 +76,19 @@
   };
 </script>
 
-<style lang="stylus">
-  .ball-container .ball position: fixed left: 32px bottom: 22px z-index: 200 &.drop-transition transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41) .inner width: 16px height: 16px border-radius: 50% background: rgb(0, 160, 220) transition: all 0.4s linear
+<style scoped lang="stylus">
+  .ball-container
+      .ball
+        position: fixed
+        left: 32px
+        bottom: 22px
+        z-index: 200
+        &.drop-transition
+          transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+          .inner
+            width: 16px
+            height: 16px
+            border-radius: 50%
+            background: rgb(0, 160, 220)
+            transition: all 0.4s linear
 </style>
