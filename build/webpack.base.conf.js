6bea766c63c73c2aa47f9639295a4ac9cc5d3023
ustbhuangyi <280309453@qq.com>
2017-01-25 12:51:27 +0800

    postcss config for android

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -92,7 +92,7 @@ module.exports = {
     loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
     postcss: [
       require('autoprefixer')({
-        browsers: ['last 2 versions']
+        browsers: ['last 2 versions', 'Android >= 4.0']
       })
     ]
   }
