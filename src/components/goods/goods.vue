014dceaa55ee5d64552b6a0b630d7549263464db
contra999 <contra999@live.cn>
2017-07-09 00:50:48 +0800

    添加注释

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -141,6 +141,7 @@
         });
 
         this.foodsScroll.on('scroll', (pos) => {
+          // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时）
           if (pos.y <= 0) {
             this.scrollY = Math.abs(Math.round(pos.y));
           }
