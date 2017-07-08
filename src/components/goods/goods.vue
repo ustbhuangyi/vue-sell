d2f07c7c9e1f3ed8ad7ed4ef38f57f53738fb641
contra999 <contra999@live.cn>
2017-07-09 01:42:45 +0800

    添加注释

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -141,7 +141,7 @@
         });
 
         this.foodsScroll.on('scroll', (pos) => {
-          // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时）
+          // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
           if (pos.y <= 0) {
             this.scrollY = Math.abs(Math.round(pos.y));
           }
