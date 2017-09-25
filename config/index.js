5ab84315dbbb01a4991916516c9126c31ddadc8e
ustbhuangyi <280309453@qq.com>
2017-09-26 02:00:43 +0800

    m build config

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
 		index: path.resolve(__dirname, '../dist/index.html'),
 		assetsRoot: path.resolve(__dirname, '../dist'),
 		assetsSubDirectory: 'static',
-		assetsPublicPath: '/',
+		assetsPublicPath: '',
 		productionSourceMap: false,
 		// Gzip off by default as many popular static hosts such as
 		// Surge or Netlify already gzip all static assets for you.
@@ -15,7 +15,7 @@ module.exports = {
 		// npm install --save-dev compression-webpack-plugin
 		productionGzip: false,
 		productionGzipExtensions: ['js', 'css'],
-		port: 9000
+		port: 8900
 	},
 	dev: {
 		env: require('./dev.env'),
