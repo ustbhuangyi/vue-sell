0ca15bf8c8f30adab9ee30ec9eaa128c13ae2c75
ustbhuangyi <280309453@qq.com>
2017-12-20 18:26:59 +0800

    fix build config

--- config/index.js
+++ config/index.js
@@ -54,7 +54,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
