5933c92bd4e6c60b86b9ff50f3f81cfacfff46d1
ustbhuangyi <280309453@qq.com>
2018-11-03 15:56:21 +0800

    add baseUrl

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import { get } from './helpers'
 
-const getSeller = get('/api/seller')
-const getGoods = get('/api/goods')
-const getRatings = get('/api/ratings')
+const getSeller = get('api/seller')
+const getGoods = get('api/goods')
+const getRatings = get('api/ratings')
 
 export {
   getSeller,
